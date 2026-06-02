e29688e67987d92b99a8c850ee66b816320ff832
zaidansyrf <zaidansyarif47@gmail.com>
2026-06-02 10:13:11 +0700

    change card style

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import {Post} from "@/types/Post";
 export async function getLastPost():Promise<Post[]>{
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
   const posts =  await res.json();
   return posts;
 }
